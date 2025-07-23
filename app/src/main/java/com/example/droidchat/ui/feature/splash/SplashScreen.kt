8ee6ed52cbb1ad15f47f820c3d54e1aea9a329b9
Bruno Nascimento <brunowcnascimento@gmail.com>
2025-07-22 23:03:26 -0300

    update res

--- app/src/main/java/com/example/droidchat/ui/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/example/droidchat/ui/feature/splash/SplashScreen.kt
@@ -1,7 +1,12 @@
 package com.example.droidchat.ui.feature.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.droidchat.R
 
 @Composable
 fun SplashRoute() {
@@ -10,7 +15,16 @@ fun SplashRoute() {
 
 @Composable
 fun SplashScreen() {
+    Column {
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = null
+        )
 
+        Row {
+
+        }
+    }
 }
 
 @Preview
