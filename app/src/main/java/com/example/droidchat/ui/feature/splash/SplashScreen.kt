3f2ec73d4ca9ecfd12e5aabca05c6b7ca0c95909
Bruno Nascimento <brunowcnascimento@gmail.com>
2025-07-22 22:59:57 -0300

    Create SplashScreen.kt

---
+++ app/src/main/java/com/example/droidchat/ui/feature/splash/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.droidchat.ui.feature.splash
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SplashRoute() {
+
+}
+
+@Composable
+fun SplashScreen() {
+
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    SplashScreen()
+}
+
