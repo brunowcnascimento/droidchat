b0f75d779849f2c223641dc617cf5d5d8bb7cec6
Bruno Nascimento <brunowcnascimento@gmail.com>
2025-07-24 22:21:49 -0300

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlinKsp)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
