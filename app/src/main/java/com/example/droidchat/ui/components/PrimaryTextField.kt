0e12e7f29c360961231ae7424c9800221089071b
Bruno Nascimento <brunowcnascimento@gmail.com>
2025-07-27 20:10:19 -0300

    Update PrimaryTextField.kt

--- app/src/main/java/com/example/droidchat/ui/components/PrimaryTextField.kt
+++ app/src/main/java/com/example/droidchat/ui/components/PrimaryTextField.kt
@@ -2,6 +2,7 @@ package com.example.droidchat.ui.components
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -18,9 +20,14 @@ import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -38,8 +45,11 @@ fun PrimaryTextField(
     placeholder: String,
     @DrawableRes leadingIcon: Int? = null,
     keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
     errorMessage: String? = null,
 ) {
+    var passwordVisibility by remember { mutableStateOf(false) }
+
     Column(
         modifier = modifier
     ) {
@@ -49,6 +59,10 @@ fun PrimaryTextField(
             modifier = modifier.fillMaxWidth(),
             singleLine = true,
             shape = CircleShape,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = keyboardType,
+                imeAction = imeAction
+            ),
             colors = OutlinedTextFieldDefaults.colors(
                 unfocusedContainerColor = MaterialTheme.colorScheme.surface,
                 focusedContainerColor = MaterialTheme.colorScheme.surface,
@@ -61,17 +75,28 @@ fun PrimaryTextField(
                 },
             ),
             visualTransformation = if(keyboardType == KeyboardType.Password) {
-                PasswordVisualTransformation()
+                if(passwordVisibility) {
+                    VisualTransformation.None
+                } else {
+                    PasswordVisualTransformation()
+                }
             } else {
                 VisualTransformation.None
             },
             trailingIcon = {
                 if(keyboardType == KeyboardType.Password && value.isNotEmpty()) {
+                    val visibilityIcon = if(passwordVisibility) {
+                        R.drawable.ic_visibility
+                    } else {
+                        R.drawable.ic_visibility_off
+                    }
                     Icon(
-                        painter = painterResource(R.drawable.ic_visibility_off),
+                        painter = painterResource(id = visibilityIcon),
                         contentDescription = null,
-                        tint = MaterialTheme.colorScheme.primary
-
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.clickable {
+                            passwordVisibility = !passwordVisibility
+                        }
                     )
                 }
             },
