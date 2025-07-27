f671d0be20e6465bcdd6d7f075ff63b87582d802
Bruno Nascimento <brunowcnascimento@gmail.com>
2025-07-26 21:43:51 -0300

    Update ChatNavHost.kt

--- app/src/main/java/com/example/droidchat/navigation/ChatNavHost.kt
+++ app/src/main/java/com/example/droidchat/navigation/ChatNavHost.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.droidchat.ui.feature.signIn.SignInRoute
 import com.example.droidchat.ui.feature.splash.SplashRoute
 import kotlinx.serialization.Serializable
 
@@ -22,11 +23,15 @@ fun ChatNavHost() {
 
     NavHost(navController = navController, startDestination = SplashRoute) {
         composable<SplashRoute> {
-            SplashRoute()
+            SplashRoute(
+                onNavigateToSignIn = {
+                    navController.navigate(SignUpRoute)
+                }
+            )
         }
 
         composable<SignInRoute> {
-            
+            SignInRoute()
         }
 
         composable<SignUpRoute> {
