6e46575332e70c52d763213c9c54a26227adbd9b
Bruno Nascimento <brunowcnascimento@gmail.com>
2025-07-26 21:43:55 -0300

    Update SplashScreen.kt

--- app/src/main/java/com/example/droidchat/ui/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/example/droidchat/ui/feature/splash/SplashScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,10 +24,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.droidchat.R
 import com.example.droidchat.ui.theme.BackgroundGradient
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashRoute() {
+fun SplashRoute(
+    onNavigateToSignIn: () -> Unit,
+) {
     SplashScreen()
+    LaunchedEffect(Unit) {
+        delay(2000)
+        onNavigateToSignIn.invoke()
+    }
 }
 
 @Composable
