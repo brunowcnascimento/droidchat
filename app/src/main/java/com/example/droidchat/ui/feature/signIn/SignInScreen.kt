cdb13bac6739c589e2decdd2419b8a3a27a69d3a
Bruno Nascimento <brunowcnascimento@gmail.com>
2026-04-27 22:53:44 -0300

    Finalizando implementação do Primary Button

--- app/src/main/java/com/example/droidchat/ui/feature/signIn/SignInScreen.kt
+++ app/src/main/java/com/example/droidchat/ui/feature/signIn/SignInScreen.kt
@@ -7,7 +7,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -22,6 +26,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.droidchat.R
+import com.example.droidchat.ui.components.PrimaryButton
 import com.example.droidchat.ui.components.PrimaryTextField
 import com.example.droidchat.ui.theme.BackgroundGradient
 import com.example.droidchat.ui.theme.DroidChatTheme
@@ -35,21 +40,24 @@ fun SignInRoute() {
 fun SignInScreen() {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var isLoading by remember { mutableStateOf(false) }
 
     Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .background(brush = BackgroundGradient)
+            .imePadding(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
 
-        ) {
+    ) {
         Image(
             painter = painterResource(R.drawable.logo),
             contentDescription = null
         )
 
-        Spacer(modifier = Modifier.height(64.dp))
+        Spacer(modifier = Modifier.height(78.dp))
 
         PrimaryTextField(
             value = email,
@@ -59,21 +67,30 @@ fun SignInScreen() {
             placeholder = stringResource(R.string.feature_login_email),
             leadingIcon = R.drawable.ic_envelope,
             keyboardType = KeyboardType.Email,
-            modifier = Modifier.padding(horizontal = 16.dp)
+            modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.spacing_medium))
         )
 
-        Spacer(modifier = Modifier.height(32.dp))
+        Spacer(modifier = Modifier.height(14.dp))
 
         PrimaryTextField(
             value = password,
             placeholder = stringResource(R.string.feature_login_password),
             leadingIcon = R.drawable.ic_lock,
             keyboardType = KeyboardType.Password,
-            modifier = Modifier.padding(horizontal = 16.dp),
+            modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.spacing_medium)),
             imeAction = ImeAction.Done,
             onValueChange = {
                 password = it
             }
+        )
+
+        Spacer(modifier = Modifier.height(98.dp))
+
+        PrimaryButton(
+            text = stringResource(id = R.string.feature_login_button),
+            modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.spacing_medium)),
+            onClick = { isLoading = !isLoading },
+            isLoading = isLoading,
         )
     }
 }
